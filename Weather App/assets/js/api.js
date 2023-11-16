fa61724057dd7f98b288ee7a066827c4cc582903
Tomáš Dvořák <150935816+Dvorinka@users.noreply.github.com>
2023-11-16 21:27:52 +0100

    Update api.js

--- Weather App/assets/js/api.js
+++ Weather App/assets/js/api.js
@@ -22,7 +22,7 @@ export const url = {
     return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
   },
   airPollution(lat, lon) {
-    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
   },
   reverseGeo(lat, lon) {
     return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
