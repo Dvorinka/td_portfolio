19942cf42c50765c8f241ef9fa03e6c8bb846020
Tomáš Dvořák <150935816+Dvorinka@users.noreply.github.com>
2023-12-12 22:29:21 +0100

    Add files via upload

---
+++ Movie/assets/js/api.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const api_key = '054582e9ee66adcbe911e0008aa482a8';
+const imageBaseURL = 'https://image.tmdb.org/t/p/';
+
+/**
+ * fetch data from a server using the `url` and passes
+ * the result in JSON data to a `callback` function,
+ * along with an optional parameter if has `optionalParam`.
+ */
+
+const fetchDataFromServer = function (url, callback, optionalParam) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => callback(data, optionalParam));
+}
+
+export { imageBaseURL, api_key, fetchDataFromServer };
