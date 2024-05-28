4518625691e3bae44d0fd13e8ff30856881caf37
Tomáš Dvořák <150935816+Dvorinka@users.noreply.github.com>
2024-05-28 09:19:27 +0200

    Update app.js

--- Weather_Project/assets/js/app.js
+++ Weather_Project/assets/js/app.js
@@ -250,7 +250,7 @@ export const updateWeather = function (lat, lon) {
 
       for (const [index, data] of forecastList.entries()) {
 
-        if (index > 12) break;
+        if (index > 7) break;
 
         const {
           dt: dateTimeUnix,
