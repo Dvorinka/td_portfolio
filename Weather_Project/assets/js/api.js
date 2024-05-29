461c3d6671343fe5fd0ca7c816d9699ee53c0ec4
Tomáš Dvořák <150935816+Dvorinka@users.noreply.github.com>
2024-05-29 11:46:15 +0200

    Update api.js

--- Weather_Project/assets/js/api.js
+++ Weather_Project/assets/js/api.js
@@ -13,7 +13,7 @@ export const url = {
     return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
   },
   forecast(lat, lon) {
-    return `https://pro.openweathermap.org/data/2.5/forecast/hourly?${lat}&${lon}&units=metric`
+    return `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={e7704bc895b4a8d2dfd4a29d404285b6}`
   },
   airPollution(lat, lon) {
     return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
